1e67197ddc8f913ff619cd8903a6b6fef0c5c38d
nyeroni <nir2y@naver.com>
2024-01-06 04:44:16 +0900

    Update: 파라미터를 Map으로 조회하기 - requestParamMap

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -102,6 +103,17 @@ public class RequestParamController {
             @RequestParam(required = false, defaultValue = "-1") int age){
 
         log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String , Object> paramMap){
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
         return "ok";
     }
 }
