aaa6e25997961fa0086248ccb9e839bc566318a9
nyeroni <nir2y@naver.com>
2024-01-05 21:50:13 +0900

    Refactor: 주석 제거

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller //컴포넌트 스캔을 통해 스프링 빈으로 등록
+@Controller
 public class SpringMemberFormControllerV1 {
 
     @RequestMapping("/springmvc/v1/members/new-form")
