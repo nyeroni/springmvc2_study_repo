921fe3fdae04ec9f9c86690820565be6c1f78650
nyeroni <nir2y@naver.com>
2024-01-06 14:25:50 +0900

    Update: @RequestBody 문자 변환

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -38,4 +38,21 @@ public class RequestBodyJsonController {
         response.getWriter().write("ok");
     }
 
+
+    /**
+     * @RequestBody
+     * HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * @ResponseBody
+     * - 모든 메서드에 @ResponseBody 적용
+     * - 메시지 바디 정보 직접 반환(view 조회 X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
+    @ResponseBody
+    @PostMapping("/request-body-json-v2")
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException{
+        HelloData data = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
 }
