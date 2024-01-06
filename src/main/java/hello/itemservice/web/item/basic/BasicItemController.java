348f919714c6e6f382814c53aee585ca08cfa5c8
nyeroni <nir2y@naver.com>
2024-01-06 22:45:29 +0900

    Update: ModelAttribute 전체 생략

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -63,14 +63,24 @@ public class BasicItemController {
 //        //model.addAttribute("item", item); //자동 추가, 생략 가능
 //        return "basic/item";
 //    }
+//
+//    /**
+//     * @ModelAttribute name 생략 가능
+//     * model.addAttribute(item); 자동 추가, 생략 가능
+//     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+//     */
+//    @PostMapping("/add")
+//    public String addItemV3(@ModelAttribute Item item){
+//        itemRepository.save(item);
+//        return "basic/item";
+//    }
 
     /**
-     * @ModelAttribute name 생략 가능
-     * model.addAttribute(item); 자동 추가, 생략 가능
-     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
      */
     @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item){
+    public String addItemV4(Item item){
         itemRepository.save(item);
         return "basic/item";
     }
