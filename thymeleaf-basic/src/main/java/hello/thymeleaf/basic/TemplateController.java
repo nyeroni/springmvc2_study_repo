c2747de89acaab20b852758b8f7d1fb61a878446
nyeroni <nir2y@naver.com>
2024-01-10 01:00:14 +0900

    Update: 템플릿 레이아웃 확장 예제

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -16,4 +16,9 @@ public class TemplateController {
     public String layout(){
         return "template/layout/layoutMain";
     }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtends(){
+        return "template/layoutExtend/layoutExtendMain";
+    }
 }
