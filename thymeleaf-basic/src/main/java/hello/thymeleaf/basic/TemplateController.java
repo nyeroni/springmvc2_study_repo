482a69ac85c90dad8a0527fc3cea13912bd4a9d8
nyeroni <nir2y@naver.com>
2024-01-10 00:49:14 +0900

    Update: 템플릿 레이아웃 예제

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -12,4 +12,8 @@ public class TemplateController {
     public String template(){
         return "template/fragment/fragmentMain";
     }
+    @GetMapping("/layout")
+    public String layout(){
+        return "template/layout/layoutMain";
+    }
 }
