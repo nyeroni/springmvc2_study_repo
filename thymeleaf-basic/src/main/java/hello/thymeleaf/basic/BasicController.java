0a23e5fbbaaf030948795f7370922e67df35dc09
nyeroni <nir2y@naver.com>
2024-01-10 10:40:19 +0900

    Update: 자바스크립트 인라인 예제

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -144,4 +144,11 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("userA", 10));
+        addUser(model);
+        return "basic/javascript";
+    }
+
 }
