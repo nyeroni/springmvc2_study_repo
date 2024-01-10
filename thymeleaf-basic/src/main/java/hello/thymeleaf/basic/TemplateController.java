4dce0febd1d155c34236ebc3de690fb7638131db
nyeroni <nir2y@naver.com>
2024-01-10 10:59:57 +0900

    Update: RestMappin 설정

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -3,9 +3,10 @@ package hello.thymeleaf.basic;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequiredArgsConstructor
+@RequestMapping("/template")
 public class TemplateController {
 
     @GetMapping("/fragment")
