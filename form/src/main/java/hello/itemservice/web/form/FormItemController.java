09e350d69e6073185e97098a62e831833bd33612
nyeroni <nir2y@naver.com>
2024-01-11 03:33:16 +0900

    Update: regions 로그 출력

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -60,10 +60,11 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        log.info("redirect gogo");
         return "redirect:/form/items/{itemId}";
     }
 
