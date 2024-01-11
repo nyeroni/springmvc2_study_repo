4dc8ecb7448a0a8215eb5899fc576614c3d0a31a
nyeroni <nir2y@naver.com>
2024-01-12 02:18:55 +0900

    Update: 내장된 인터페이스 사용

--- message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
 import java.util.Locale;
