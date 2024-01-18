8690649e79bda2f34136c46c58abb01194ec310a
nyeroni <nir2y@naver.com>
2024-01-18 17:46:27 +0900

    Refactor: 오타 수정

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -11,7 +11,7 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
     public void customize(ConfigurableWebServerFactory factory){
 
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
-        ErrorPage errorPage500 = new ErrorPage((HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
         ErrorPage errorPageEX = new ErrorPage(RuntimeException.class, "/error-page/500");
         factory.addErrorPages(errorPage404, errorPage500, errorPageEX);
     }
