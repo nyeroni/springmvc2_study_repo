a9194748515bd50b39ecac2de22e7d4c27c52a7f
nyeroni <nir2y@naver.com>
2024-01-19 13:10:08 +0900

    Feat: @RequestParam 이용해서 입력받은 data 값을 숫자로 타입 변환

--- typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -2,6 +2,7 @@ package hello.typeconverter.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,12 @@ public class HelloController {
         String data = request.getParameter("data");//문자 타입 조회
         Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
         System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        System.out.println("data = "+ data);
         return "ok";
     }
 }
